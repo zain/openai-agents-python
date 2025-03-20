9384a0fb3fd13151c010d3f45c89bfcb05172784
xianghuijin <xianghuijin@bambooai.top>
2025-03-20 21:28:24 +0800

    Revert test file name to match project naming style

--- tests/test_tool_choice_reset.py
+++ tests/test_tool_choice_reset.py
@@ -54,7 +54,7 @@ class MockModel:
         )
 
 
-class TestPreventInfiniteToolLoop:
+class TestToolChoiceReset:
     async def test_tool_choice_resets_after_call(self):
         """Test that tool_choice is reset to 'auto' after tool call when set to 'required'"""
         # Create an agent with tool_choice="required"
