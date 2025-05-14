02b6e7013cf00de92b453610f459be40f344e6a8
leohpark <135409779+leohpark@users.noreply.github.com>
2025-05-14 09:37:06 -0700

    Update search_agent.py (#677)
    Added missing word "be" in prompt instructions.
    
    This is unlikely to change the agent functionality in most cases, but
    optimal clarity in prompt language is a best practice.

--- examples/research_bot/agents/search_agent.py
+++ examples/research_bot/agents/search_agent.py
@@ -3,7 +3,7 @@ from agents.model_settings import ModelSettings
 
 INSTRUCTIONS = (
     "You are a research assistant. Given a search term, you search the web for that term and "
-    "produce a concise summary of the results. The summary must 2-3 paragraphs and less than 300 "
+    "produce a concise summary of the results. The summary must be 2-3 paragraphs and less than 300 "
     "words. Capture the main points. Write succinctly, no need to have complete sentences or good "
     "grammar. This will be consumed by someone synthesizing a report, so its vital you capture the "
     "essence and ignore any fluff. Do not include any additional commentary other than the summary "
